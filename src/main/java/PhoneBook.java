f56a218efbc2526508cf225384b55f6c5b053a8d
Vera Knyazeva <verashibanova@yandex.ru>
2024-08-12 10:09:18 +0500

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,19 @@
-public class PhoneBook {
+import java.util.Map;
+import java.util.TreeMap;
 
-    public int add(String name, String phoneNumber){
-        return 0;
+public class PhoneBook {
+    private final Map<String, String> contacts = new TreeMap<>();
+    private final Map<String, String> contactsByPhone = new TreeMap<>();
+
+    public int add(String name, String phoneNumber) {
+        if (name == null || phoneNumber == null) {
+            throw new IllegalArgumentException();
+        }
+        contacts.put(name, phoneNumber);
+        contactsByPhone.put(phoneNumber, name);
+
+        return contacts.size();
     }
+
+
 }
